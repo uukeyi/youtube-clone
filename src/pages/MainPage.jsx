b55ef033cca1ffd8b726bbc0be39dbdddbcfde8b
Aleksandr <119818692+uukeyi@users.noreply.github.com>
2024-02-01 16:33:47 +0300

    initial commit with base structure

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+
+const MainPage = () => {
+    return (
+        <>
+        <Header/>
+     
+        </>
+    )
+}
+export default MainPage;
